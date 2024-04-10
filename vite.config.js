b2a8ee9d063546cf2a9273a00a18e18e4c2470e5
devBasitAli <132766566+DevBasitali@users.noreply.github.com>
2024-04-10 21:37:52 +0500

    dist

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'build', // Specify the output directory here
+    outDir: 'dist', // Output directory should be 'dist'
   },
   plugins: [react()],
 })
